560b090c584cf7d93c37af6f5449a21a1e2f10ce
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-06-25 22:56:38 +0530

    Update Solve Me First.java

--- Easy/Solve Me First.java
+++ Easy/Solve Me First.java
@@ -24,3 +24,35 @@ public class Solution {
         System.out.println(sum);
    }
 }
+
+
+
+
+
+
+
+
+
+
+// OR
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int a, b, sum;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println(sum);
+    
+    }
+}
